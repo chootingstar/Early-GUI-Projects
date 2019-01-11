ad89b9b65a4ff2de8e88df78f16c79f7ce4cbcb4
chootingstar <jmirayes96@gmail.com>
2019-01-11 14:21:42 -0800

    Better GUIs

---
+++ GGGUIWorks.java
@@ -0,0 +1,41 @@
+public class GGGUIWorks extends JFrame{
+
+	//public variables
+
+	
+
+	public something GUI constructor {
+
+	//Has the basis of the gui
+
+	}
+
+	
+
+	public void random(){
+
+	//Generates random number
+
+	}
+
+	
+
+	public void answerCheck() {
+
+	//calls random
+
+	//prompts user to guess the number
+
+	//if-else statements going over the answer given by the user
+
+	}
+
+	
+
+	public static void main(){
+
+	//initiates the game
+
+	//When user succeeds displays success
+
+	}
